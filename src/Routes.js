e39e3b8bad81a43a45795e5c2dad6c2f4f9e060e
Luan Bitar <bitar.luan21@gmail.com>
2019-03-22 15:17:36 -0400

    adding animation on change route

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,21 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
-
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { AnimatedSwitch } from 'react-router-transition'
 import Home from 'pages/Home'
 import Engine from './pages/Engine'
 
 const Routes = () => (
-  <BrowserRouter>
-    <Route path="/" component={Home} exact />
-    <Route path="/engine" component={Engine} />
-  </BrowserRouter>
+  <Router>
+    <AnimatedSwitch
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 0 }}
+        atActive={{ opacity: 1 }}
+        className="switch-wrapper"
+      >
+        <Route path="/" component={Home} exact />
+        <Route path="/engine" component={Engine} />
+      </AnimatedSwitch>
+  </Router>
 )
 
 export default Routes
