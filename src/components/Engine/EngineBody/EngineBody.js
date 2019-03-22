981b8ec601ac5235f0752bc404708ebb4ad77b3f
Luan Bitar <bitar.luan21@gmail.com>
2019-03-22 14:29:16 -0400

    Adding engine images

--- src/components/Engine/EngineBody/EngineBody.js
+++ src/components/Engine/EngineBody/EngineBody.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
 import s from './EngineBody.css'
+import withoutEngine from 'images/with-engine.png'
+import withEngine from 'images/without-engine.png'
 
 const EngineBody = () => (
   <div className={s.container}>
-  
+    <div className={s.section}>
+      <img src={withoutEngine} alt="Car without engine" />
+    </div>
+    <div className={[s.section, s.radioSection].join(' ')}>
+      <img src={withEngine} alt="Car with engine" />
+    </div>
   </div>
 )
 
