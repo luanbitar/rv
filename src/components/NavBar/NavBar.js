ae47cfaef04035388f0f1e608046518655275d95
Luan Bitar <bitar.luan21@gmail.com>
2019-03-22 21:42:48 -0400

    adding sass style

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,31 +1,31 @@
 import React from 'react'
 
-import s from './NavBar.css'
+import s from './NavBar.scss'
 import RVLogo from 'images/RV-logo.svg'
 import { Link } from 'react-router-dom'
 
 const NavBar = () => (
   <div className={s.container}>
-    <div className={s.limitWidthContainer}>
+    <div className="limitWidthContainer">
       <a href="https://www.redventures.com/">
         <img src={RVLogo} alt="Red Ventures logo" />
       </a>
-      <ul className={s.menu}>
-        <li className={[s.option, s.active].join(' ')}>
+      <ul className="menu">
+        <li className="option active">
           <Link to="/">
             Model R
           </Link>
         </li>
-        <li className={s.option}>
+        <li className="option">
           Model IQ
         </li>
-        <li className={s.option}>
+        <li className="option">
           Model Mobi
         </li>
-        <li className={s.option}>
+        <li className="option">
           Model Charlie
         </li>
-        <li className={s.option}>
+        <li className="option">
           Model Italy
         </li>
       </ul>
