075dec7b6c7a50c9742b997233061b733d8e612e
Luan Bitar <bitar.luan21@gmail.com>
2019-03-24 01:11:29 -0400

    Reduce min-width responsive support to 360px

--- src/pages/Color.js
+++ src/pages/Color.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 import NavBar from 'components/NavBar/NavBar'
-import EngineBody from 'components/EngineBody/EngineBody'
+
 import Footer from 'components/Footer/Footer'
 
 const Engine = () => (
   <div className="main--container">
     <NavBar />
-    <EngineBody />
+
     <Footer />
   </div>
 )
