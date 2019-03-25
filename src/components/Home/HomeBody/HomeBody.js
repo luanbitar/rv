9e20155c9985e87de0d4c9f033f2accb9a9907db
Luan Bitar <bitar.luan21@gmail.com>
2019-03-25 14:03:02 -0400

    Move container reponsability to page

--- src/components/Home/HomeBody/HomeBody.js
+++ src/components/Home/HomeBody/HomeBody.js
@@ -1,41 +1,21 @@
-import React, { Component } from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux';
+import React from 'react'
 
 import s from './HomeBody.scss'
 import CarImage from 'images/car-home.png'
 import ButtonNext from 'components/ButtonNext/ButtonNext'
-import { fetchData } from 'utils/fetch'
-import * as carActions from 'reducers/Car/actionCreators'
 
-class HomeBody extends Component {
+const HomeBody = () => 
+  <div className={s.container}>
+  <div className="section">
+    <h2 className="title">Build your</h2>
+    <h1 className="subTitle">Model <span>R</span></h1>
+  </div>
+  <img className="carImage" src={CarImage} alt="Car red" />
+  <div className="btn">
+    <ButtonNext to="/engine">
+      <span className="advance">Begin</span>
+    </ButtonNext>
+  </div>
+  </div>
 
-  state = { data: {} }
-
-  componentDidMount = async () => {
-    const result = await fetchData()
-    this.props.getCarData(result.data)
-    console.log(this.props)
-
-  }
-
-  render = () =>
-    <div className={s.container}>
-      <div className="section">
-        <h2 className="title">Build your</h2>
-        <h1 className="subTitle">Model <span>R</span></h1>
-      </div>
-      <img className="carImage" src={CarImage} alt="Car red" />
-      <div className="btn">
-        <ButtonNext to="/engine">
-          <span className="advance">Begin</span>
-        </ButtonNext>
-      </div>
-    </div>
-}
-
-const mapStateToProps = state => ({ carData: state.CarReducer })
-
-const mapDispatchToProps = dispatch => bindActionCreators(carActions, dispatch)
-
-export default connect(mapStateToProps, mapDispatchToProps)(HomeBody)
+export default HomeBody
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
 import NavBar from 'components/NavBar/NavBar'
 import HomeBody from 'components/Home/HomeBody/HomeBody'
-import HomeFooter from '../components/Home/HomeFooter/HomeFooter';
+import HomeFooter from '../components/Home/HomeFooter/HomeFooter'
+import { fetchData } from 'utils/fetch'
+import * as carActions from 'reducers/Car/actionCreators'
 
-const Home = () => (
-  <div className="main--container">
-    <NavBar />
-    <HomeBody />
-    <HomeFooter />
-  </div>
-)
+class Home extends Component {
 
-export default Home
+  state = {  }
+
+  componentDidMount = async () => {
+    const result = await fetchData()
+    this.props.getCarData(result.data)
+    console.log(this.props)
+  }
+
+  render = () => 
+    <div className="main--container">
+      <NavBar />
+      <HomeBody />
+      <HomeFooter />
+    </div>
+}
+
+const mapStateToProps = state => ({ carData: state.CarReducer })
+
+const mapDispatchToProps = dispatch => bindActionCreators(carActions, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
